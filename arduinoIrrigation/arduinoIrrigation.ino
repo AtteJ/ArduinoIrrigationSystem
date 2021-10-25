2fdc9abd161979c1f0df72df0c43cb74296eed4e
AtteJ <attejokinen09@gmail.com>
2021-10-25 18:00:34 +0300

    tweaks

--- arduinoIrrigation/arduinoIrrigation.ino
+++ arduinoIrrigation/arduinoIrrigation.ino
@@ -2,6 +2,7 @@ const int RELAY_PIN = A5;
 const int SENSOR_PIN = A4;
 
 const int MOISTURE_LIMIT = 300;
+const int WATERING_TIME = 1500;
 
 float OUTPUT_VALUE;
 
@@ -12,24 +13,32 @@ void setup() {
 
 void loop()
 {
+  OUTPUT_VALUE = get_moisture();
+  
+  if (OUTPUT_VALUE < MOISTURE_LIMIT)
+    pump_water(WATERING_TIME);
+  
+  Serial.println(OUTPUT_VALUE);
+  delay(10000);
+}
+
+
+float get_moisture() {
   for (int i = 0; i <= 100; i++)
   {
     OUTPUT_VALUE = OUTPUT_VALUE + analogRead(SENSOR_PIN);  // Reads from the soil moisture sensor
     delay(1);
   }
+  
   OUTPUT_VALUE = OUTPUT_VALUE / 100;
   float OUTPUT_PERCENT = OUTPUT_VALUE / 1024 * 100; // Converts output to percents
-  if (OUTPUT_VALUE < MOISTURE_LIMIT)
-    pump_water();
-  
-  // Serial.println(OUTPUT_VALUE);
-  Serial.print(OUTPUT_PERCENT);
-  Serial.println(" %");
-  delay(10000);
+
+  return OUTPUT_VALUE;
 }
 
-void pump_water() {
+
+void pump_water(int WATERING_TIME) {
   digitalWrite(RELAY_PIN, HIGH); // turn on pump 1.5 seconds
-  delay(1500);
+  delay(WATERING_TIME);
   digitalWrite(RELAY_PIN, LOW);  // turn off pump
 }
